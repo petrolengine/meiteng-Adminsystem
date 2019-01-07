d0e9dcc065623e993f79b80cc70d2394cbb38d4c
yd <linas_public@aliyun.com>
2019-01-07 17:57:38 +0800

    refact login ui

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './Login.css';
 import '../common/css/label.css'
-import '../common/css/common.css'
 
 class Login extends Component {
     constructor(props, context) {
@@ -37,22 +36,19 @@ class Login extends Component {
 
     render() {
         return (
-            <div className="modal background">
-                <div className="modal-left" >
-                </div>
-                <div className="modal-right">
-                    <form className="modal-content" action={`${process.env.REACT_APP_URL_PREFIX}/login`} method="POST" onSubmit={this.handleSubmitEvent}>
-                        <h1 className={"login_title w20_8ch"}>登录</h1>
-                        {this.state.error && <label color="warning">{this.state.message}</label>}
-                        < label htmlFor="uname" className="loginName w14_2ch" > 账户</label>
-                        <input className="input_sty input_size" type="text" placeholder="请输入您的账户名" name="uname" required></input>
-                        <label htmlFor="psw" className="passLabel w14_2ch">密码</label>
-                        <input className="input_sty input_size" type="password" placeholder="请输入您的密码" name="psw" required></input>
-                        <button className="btn_ok" type="submit">确定</button>
-                        <input type="checkbox" name="remember" id="remember" ></input>
-                        <label htmlFor="remember" className="remeber_sty w12_1ch">记住密码</label>
-                    </form>
-                </div>
+            <div className="login_modal">
+                <div className="login_modal_left"></div>
+                <form className="login_modal_right" action={`${process.env.REACT_APP_URL_PREFIX}/login`} method="POST" onSubmit={this.handleSubmitEvent}>
+                    <h1 className={"login_title w20_8ch"}>登录</h1>
+                    {this.state.error && <label color="warning">{this.state.message}</label>}
+                    <label className="login_nmr login_label w14_2ch" > 账户</label>
+                    <input className="login_nmr login_input_sty" type="text" placeholder="请输入您的账户名" name="uname" required></input>
+                    <label className="login_nmr login_label w14_2ch">密码</label>
+                    <input className="login_nmr login_input_sty" type="password" placeholder="请输入您的密码" name="psw" required></input>
+                    <button className="login_nmr btn_ok w14_2ch" type="submit">确定</button>
+                    <input className="login_cb" type="checkbox" name="remember" id="remember" ></input>
+                    <label htmlFor="remember" className="remeber_sty w12_1ch">记住密码</label>
+                </form>
             </div >
         );
     }
