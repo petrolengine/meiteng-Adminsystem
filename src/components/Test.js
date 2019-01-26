dd0bd2e3f7453fb2fdead1c2acae979f55c74f37
Petrol <petrol.engine.com@gmail.com>
2019-01-26 17:35:23 +0800

    add landlord page

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,9 +1,54 @@
 import React from 'react';
 import '../resources/css/Test.css'
+import '../resources/css/common.css'
 
 class Test {
     get render() {
-        return (<p>hello world</p>);
+        return (
+            <div>
+                <div className="decoration"></div>
+                <div className="bg_img">
+                    <div className="specil_frame">
+                        <label className="b title_add_landlord w20_1ch">添加房东</label>
+                        <div className="info_landlord">
+                            <div className="b bk">
+                                <label className="bkf w15_2ch in_top" >姓名</label>
+                                <input className="info_landlord_content in_top"></input>
+                            </div>
+                            <div className="b bk">
+                                <label className="bkf w15_2ch in_top">性别</label>
+                                <input className="info_landlord_content in_top"></input>
+                            </div>
+                            <div className="b bk">
+                                <label className="bkf w15_2ch in_top">年龄</label>
+                                <input className="info_landlord_content in_top"></input>
+                            </div>
+                            <div className="b bk">
+                                <label className="bkf w15_2ch in_top">电话</label>
+                                <input className="info_landlord_content in_top"></input>
+                            </div>
+                            <div className="b bk">
+                                <label className="bkf w15_2ch in_top">常住地</label>
+                                <input className="info_landlord_content in_top"></input>
+                            </div>
+                            <div className="b bk">
+                                <label className="bkf w15_2ch in_top">身份证号</label>
+                                <input className="info_landlord_content in_top"></input>
+                            </div>
+                            <div className="b bk">
+                                <label className="bkf w15_2ch in_top">房屋资源</label>
+                                <input className="info_landlord_content in_top"></input>
+                            </div>
+                            <div className="b bk">
+                                <label className="bkf w15_2ch in_top">房东诉求</label>
+                                <input className="info_landlord_content in_top"></input>
+                            </div>
+                        </div>
+                        <button className="finish w15_2ch">确定</button>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
