53df3b17b40f024c7a2a2f1c5bc9980278dceaad
Petrol <petrol.engine.com@gmail.com>
2019-01-31 22:03:15 +0800

    bugfix

--- src/common/Function.js
+++ src/common/Function.js
@@ -29,7 +29,7 @@ export function renderPage(total, current) {
     items.push(<button className="goto_page_arrow_left in_top noborder goto_page_size" disabled={current === 0}></button>);
     for (let idx = 0; idx < total; idx++) {
         if (idx === current) {
-            items.push(<button className="goto_page_size in_top b15_1_ch noborder m_l_2" style={{ backgroundColor: "red" }} disabled>{idx + 1}</button>);
+            items.push(<button className="goto_page_size in_top b15_1_ch noborder m_l_2" style={{ backgroundColor: "rgb(236, 125, 65)" }} disabled>{idx + 1}</button>);
         } else {
             items.push(<button className="goto_page_size in_top b15_1_ch noborder m_l_2" style={{ backgroundColor: "#eeeeee" }}>{idx + 1}</button>);
         }
--- src/components/LandlordPage.js
+++ src/components/LandlordPage.js
@@ -19,6 +19,36 @@ export default class LandlordPage {
     }
 
     renderOneResult(obj, idx) {
+        if (idx % 2 === 1) {
+            return (
+                <div className="in_top landlord_info_card" style={{ float: "right" }} key={`landlord_item_${idx}`}>
+                    <div className="landlord_info_name in_middle">
+                        <label className="b15_1_ch in_top person_name">{obj.name}</label>
+                        <label className="gray10_1_ch in_top person_age">{obj.age + CommonStr.sui}</label>
+                        <div className="landlord_sex"></div>
+                    </div>
+                    <div className="landlord_info_icon in_top">
+                        <div className="tele_img deviation b"></div>
+                        <div className="identity_img deviation b"></div>
+                        <div className="obode_img deviation b"></div>
+                        <div className="house_resourde_img deviation b"></div>
+                        <div className="commands_img deviation b"></div>
+                    </div>
+                    <div className="landlord_mian_info in_top">
+                        <label className="landlord_tele gray15_1_ch b">{CommonStr.phone + ": " + obj.phone}</label>
+                        <label className="landlord_tele gray15_1_ch b">{CommonStr.id_card + ": " + obj.id_card}</label>
+                        <label className="landlord_tele gray15_1_ch b">{CommonStr.native_place + ": " + obj.native_place}</label>
+                        <label className="landlord_tele gray15_1_ch b">{CommonStr.resources + ": "}
+                            <label className="b15_1_ch in_top">{CommonStr.yizu + "   " + CommonStr.yishou + "   " + CommonStr.daizu + "   " + CommonStr.daishou}</label>
+                            <label className="b15_1_ch in_top house_num">{obj.yizu + "   " + obj.yishou + "   " + obj.daizu + "   " + obj.daishou}</label>
+                        </label>
+                        <label className="landlord_tele gray15_1_ch in_top">{CommonStr.remarks + ": " + obj.remarks}</label>
+                    </div>
+                    <button className="landlord_edit"></button>
+                    <label className="landlord_last_time gray10_1_ch">{obj.datetime}</label>
+                </div >
+            );
+        }
         return (
             <div className="in_top landlord_info_card" key={`landlord_item_${idx}`}>
                 <div className="landlord_info_name in_middle">
